916f7f1b2a727ea5379f1326f7129c64c6d2703c
Pramod Kumar Voola <Pramod@Pummy-cbz.home>
2020-01-26 18:43:35 -0500

    DojoSurvery Assignment Completed

---
+++ Python_Stack/flask/flask_fundamentals/dojo_survey/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/results', methods=['POST','GET'])
+def result():
+    results_captured = request.form
+    print(results_captured.getlist('food'))
+    return render_template("results.html", results_captured = request.form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
