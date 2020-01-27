710896499781931051d5e674cfebf29e7b983f0c
Pramod Kumar Voola <Pramod@Pummy-cbz.home>
2020-01-27 00:42:36 -0500

    updated Form_test server code to use sessions

--- Python_Stack/flask/flask_fundamentals/form_test/server.py
+++ Python_Stack/flask/flask_fundamentals/form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 app = Flask(__name__)
+app.secret_key = "b'\x1c\xa8\\T\xa2k%\x8c\xc6\xaf\xfc\xa1!l\xeb\xcc'"
 
 # Index route
 
@@ -11,9 +12,14 @@ def index():
 def create_user():
     print("Got Post Info")
     print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
-    return render_template("show.html",name_on_template= name_from_form, email_on_template=email_from_form)
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+    # return render_template("show.html",name_on_template= name_from_form, email_on_template=email_from_form)
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
 
 
 
