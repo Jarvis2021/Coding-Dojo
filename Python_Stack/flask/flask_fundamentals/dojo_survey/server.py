b0b97598671febd6f094e2c5f221009564197c6e
Pramod Kumar Voola <Pramod@Pummy-cbz.home>
2020-01-26 19:11:54 -0500

    updated code for results.html to include the lists

--- Python_Stack/flask/flask_fundamentals/dojo_survey/server.py
+++ Python_Stack/flask/flask_fundamentals/dojo_survey/server.py
@@ -8,8 +8,6 @@ def index():
 
 @app.route('/results', methods=['POST','GET'])
 def result():
-    results_captured = request.form
-    print(results_captured.getlist('food'))
     return render_template("results.html", results_captured = request.form)
 
 if __name__ == '__main__':
